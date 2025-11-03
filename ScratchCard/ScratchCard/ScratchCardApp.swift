bdc9d10432f9225c1bb33bda736b2ec080a70ae7
Marek Hajdučák <m.hajducak.extern@rewe-group.at>
2025-11-03 12:09:43 +0100

    init

---
+++ ScratchCard/ScratchCard/ScratchCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScratchCardApp.swift
+//  ScratchCard
+//
+//  Created by MacBook Pro on 03/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct ScratchCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
